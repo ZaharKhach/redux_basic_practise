e1a70e7de2aa0e49d94d9a7dcfaf5287b56b494a
dendywool <zaharkhachaturyan@gmail.com>
2023-09-16 15:41:09 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, bindActionCreators } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+
+import App from './Components/App';
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+/////////TEST//////////TEST////////TEST/////////TEST//////////TEST////////TEST/////////TEST//////////TEST////////TEST
+// const initialState = {
+//   value: 0
+// };
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'INC':
+//       return {
+//         ...state,
+//         value: state.value + 1
+//       };
+//     case 'DEC':
+//       return {
+//         ...state,
+//         value: state.value - 1
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// const store = createStore(reducer);
+
+// const inc = () => {
+//   return {
+//     type: 'INC'
+//   };
+// };
+
+// const dec = () => {
+//   return {
+//     type: 'DEC'
+//   };
+// };
+
+// const { incDispatch, decDispatch } = bindActionCreators(
+//   {
+//     incDispatch: inc,
+//     decDispatch: dec
+//   },
+//   store.dispatch
+// );
+
+// store.subscribe(() => {
+//   document.querySelector('.result').textContent = store.getState().value;
+// });
+
+// const Test = () => {
+//   return (
+//     <div>
+//       <div className='result'></div>
+//       <button className='btn btn-primary' onClick={incDispatch}>
+//         INC
+//       </button>
+//       <button className='btn btn-primary' onClick={decDispatch}>
+//         DEC
+//       </button>
+//     </div>
+//   );
+// };
+
+// const root = document.getElementById('root');
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <Test />
+//     </React.StrictMode>
+//   </Provider>,
+//   root
+// );
+
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args))
+// }
+
+// const decDispatch = bindActionCreator(dec, dispatch);
+// const rndDispatch = bindActionCreator(rnd, dispatch);
+
+
+// document.querySelector('#inc').addEventListener('click', inc)
+
+// document.querySelector('#dec').addEventListener('click', dec)
+
+// document.querySelector('#rnd').addEventListener('click', () => {
+//   const value = Math.floor(Math.random() * 100);
+//   rnd(value)
+// })
+
+
+
