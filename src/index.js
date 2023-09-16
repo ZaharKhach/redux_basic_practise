a753b284ebe6e03ca36bf36906ca0b4b96f3a19a
dendywool <zaharkhachaturyan@gmail.com>
2023-09-16 15:46:49 +0200

    empty code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, bindActionCreators } from 'redux';
-import reducer from './reducer';
-import { Provider } from 'react-redux';
 
-import App from './Components/App';
 
-const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+
   </React.StrictMode>
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /////////TEST//////////TEST////////TEST/////////TEST//////////TEST////////TEST/////////TEST//////////TEST////////TEST
